6b0aa301491c95aab01c19b5dd3deb251c5b538b
Nicolas vauché <hello@nicolasvauche.net>
2024-04-12 11:42:55 +0200

    Authentification avec LoginForm + JWT

--- src/Controller/Api/ProductsController.php
+++ src/Controller/Api/ProductsController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/products', name: 'app_api_products_')]
 class ProductsController extends AbstractController
 {
-    #[Route('/', name: 'index', methods: ['GET'])]
+    #[Route('', name: 'index', methods: ['GET'])]
     public function index(ProductRepository $productRepository): Response
     {
         $products = $productRepository->findAll();
